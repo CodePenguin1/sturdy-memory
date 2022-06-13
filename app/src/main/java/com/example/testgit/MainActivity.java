c03eea7e38c6fc39dc633c5c33c596921506d310
jason <jasonw1417@gmail.com>
2022-06-13 18:32:12 -0500

    Downgraded project to Android version 8.0

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txtRip;
+    Button btnBoop;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //txtRip.setText("My Awesome Text");
+        btnBoop = (Button)findViewById(R.id.btnBoop);
+        txtRip = (TextView) findViewById(R.id.txtRip);
+    }
+
+
+    public void onClick(View view) {
+            txtRip = findViewById(R.id.txtRip);
+            txtRip.setText("Hello");
     }
 }
