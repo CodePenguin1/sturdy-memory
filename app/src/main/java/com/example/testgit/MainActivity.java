e53fc0808eb474f437fea7c516b96af0850645ba
Brandon <clickedbigfoot@gmail.com>
2022-06-27 17:43:14 -0500

    Sockets still dont work but are messier now

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnBoop;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
