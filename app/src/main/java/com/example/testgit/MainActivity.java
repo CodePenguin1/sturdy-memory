fcb2c510d23b315d9149fb758d3bb10186252e67
jason <jasonw1417@gmail.com>
2022-07-11 16:13:04 -0500

    Removed unused Client/server code WIP scrollable image

--- app/src/main/java/com/example/testgit/MainActivity.java
+++ app/src/main/java/com/example/testgit/MainActivity.java
@@ -6,9 +6,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.util.Log;
 import android.widget.Toast;
@@ -18,55 +22,75 @@ public class MainActivity extends AppCompatActivity {
     TextView txtRip;
     Button btnBoop;
 
+    @SuppressLint("ClickableViewAccessibility")
+
+    /**
+     * This method sets a viewable image that's scrollable with touch input
+     * @createMap()
+     * @return()
+     * Search to get the above to show up properly in dialogue box
+     */
+    public void createMap() {
+
+
+        final ImageView switcherView = (ImageView) this.findViewById(R.id.huntMap);
+
+        switcherView.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View arg0, MotionEvent event) {
+
+                float curX, curY;
+                int mx = 0;
+                int my = 0;
+                //These are original x and y coordinates
+
+                switch (event.getAction()) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        mx = (int) event.getX();
+                        my = (int) event.getY();
+                        //x and y cords initialized
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        curX = event.getX();
+                        curY = event.getY();
+                        switcherView.scrollBy((int) (mx - curX), (int) (my - curY));
+                        //scrollBy -
+                        mx = (int) curX;
+                        my = (int) curY;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //ACTION_UP - gets current pos when finger is lifted
+                        curX = event.getX();
+                        curY = event.getY();
+                        switcherView.scrollBy((int) (mx - curX), (int) (my - curY));
+                        break;
+                }
+
+                return true;
+            }
+        });
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createMap();
+
         btnBoop = (Button)findViewById(R.id.btnBoop);
         txtRip = (TextView) findViewById(R.id.txtRip);
 
-        //txtRip.setText("My Awesome Text");
-        //Documentation is important^^^
 
-        Toast.makeText(this, "Nothing interesting happens", Toast.LENGTH_LONG).show();
-
-        TCPClient client = new TCPClient();
-        try {
-            Toast.makeText(this, client.getGameState(), Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Socket server = null;
-        BufferedReader input = null;
-        try {
-            server = new Socket("192.168.0.182", 2000);
-            input = new BufferedReader(new InputStreamReader(server.getInputStream())); // getting message from client
-        }
-        catch(Exception e) {
-            String errorMessage = e.toString();
-            if (errorMessage == null) {
-                errorMessage = "Null error message when connecting to or reading from server";
-            }
-            Log.e("Main", errorMessage);
-        }
-        try {
-            if (input != null && server != null) {
-                Log.i("Main", "Message received");
-                server.close();
-
-                Toast.makeText(this, input.readLine(),
-                        Toast.LENGTH_LONG).show();
-            }
-        }
-        catch(Exception e) {
-            Log.e("Main", "Uh oh spaghetti oh");
-        }
     }
 
+
     public void onClick(View view) {
+
         txtRip = findViewById(R.id.txtRip);
         txtRip.setText("Hello");
     }
